8dd8a7a7d773a76fab2ee71e5e743b1b18108d7b
GinishGarg <kg209675@gmail.com>
2025-01-27 01:45:25 +0530

    added Avatar to sidebar and option to logout

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,11 +1,27 @@
+"use client";
 
+import Image from "next/image";
+import { useUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
-const documentsPage = () => {
+const DocumentsPage = () => {
+  const {user} = useUser();
   return (
-    <div>
-      This is a Documents Page
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+      src="/empty-tray.png"
+      alt="Empty"
+      height="300"
+      width="300"
+      />
+      <h2 className="text-lg font-medium">Welcome to {user?.firstName}&apos;s Notion</h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2"/>
+        Create a Note...
+      </Button>
     </div>
   )
 }
 
-export default documentsPage
+export default DocumentsPage
