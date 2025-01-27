2036745fc0cd169d2e61661a560ae68b4f255a09
GinishGarg <kg209675@gmail.com>
2025-01-27 10:16:18 +0530

    added notes as a sidebar item

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,10 +6,13 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import {useMediaQuery} from "usehooks-ts"
 import UserItem from "./user-item";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px")
+    const documents = useQuery(api.documents.get)
 
     const isResizingRef = useRef(false);
     const sideBarRef = useRef<ElementRef<"aside">>(null);
@@ -105,7 +108,9 @@ const Navigation = () => {
             <UserItem />
         </div>
         <div className="mt-4">
-            <p>Documents</p>
+            <p>{documents?.map((document)=>(
+                <p key={document._id}>{document.title}</p>
+            ))}</p>
 
         </div>
         <div 
